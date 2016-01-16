2dd8f6a0f38f102863e8268b7b7224bc7589c3e3
Patryk Kalinowski <patryk.kalinowski@netguru.pl>
2016-01-16 20:20:43 +0100

    Generated sqlite database

---
+++ src/Patryk/BookBundle/Entity/Reflection.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Patryk\BookBundle\Entity;
+
+/**
+ * Reflection
+ */
+class Reflection
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $body;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Reflection
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set body
+     *
+     * @param string $body
+     *
+     * @return Reflection
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * Get body
+     *
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+}
+
